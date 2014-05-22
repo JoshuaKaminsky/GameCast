845d972b214a9a7c39e90ec6ef3e7c36b1575469
Josh Kaminsky <josh.kaminsky@gmail.com>
2014-05-22 01:05:08 -0400

    turn off authentication while it does not work

--- web/config/policies.js
+++ web/config/policies.js
@@ -16,7 +16,7 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access) 
-  '*': 'authenticated',
+  '*': true,
 
   'lounge' : {
 	'index' : true
