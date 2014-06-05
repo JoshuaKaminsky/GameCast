7609e97108517df995ab80007ea0a48918e68b2b
Josh Kaminsky <josh.kaminsky@gmail.com>
2014-06-05 13:57:38 -0400

    config changes
    Andy get my local config file

--- web/config/adapters.js
+++ web/config/adapters.js
@@ -14,43 +14,4 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'mongo',
-
-  /*
-  mongo: {
-    module   : 'sails-mongo',
-    host     : 'localhost',
-    user     : '',
-    password : '',
-    database : 'gamecastdb'
-  },
-  */
-  
-  mongo: {
-    module: 'sails-mongo',
-    url: "mongodb://gamecastuser:howaboutthebestpasswordever!@kahana.mongohq.com:10020/gamecast_staging"
-  },
-
-  // In-memory adapter for DEVELOPMENT ONLY
-  memory: {
-    module: 'sails-memory'
-  },
-
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data IS preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
-
-  // // MySQL is the world's most popular relational database.
-  // // Learn more: http://en.wikipedia.org/wiki/MySQL
-  // mysql: {
-  //   module: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER',
-  //   password: 'YOUR_MYSQL_PASSWORD',
-  //   database: 'YOUR_MYSQL_DB'
-  // }
 };
