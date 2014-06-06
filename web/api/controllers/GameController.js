aab40207fd3cbf99129e99f3ddd141b9b00a7285
Andy Fedus <andyfedus@gmail.com>
2014-06-05 20:19:55 -0400

    Fix indentation b/c Josh is bad

--- web/api/controllers/GameController.js
+++ web/api/controllers/GameController.js
@@ -1,43 +1,43 @@
-/**
- * GameController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
+  /**
+   * GameController
+   *
+   * @module      :: Controller
+   * @description	:: A set of functions called `actions`.
+   *
+   *                 Actions contain code telling Sails how to respond to a certain type of request.
+   *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+   *
+   *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+   *                 and/or override them with custom routes (`config/routes.js`)
+   *
+   *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+   *
+   * @docs        :: http://sailsjs.org/#!documentation/controllers
+   */
 
-module.exports = {
+   module.exports = {
     
-   game: function(req, res) {
-       // Request includes the game ID, let's find it.
-       Game.findOne( { 'id': req.param('id') }).done(
-       	    function(err, game) {
-       	    	if (err) {
-       	    		return console.log(err)
-       	    	}
+     game: function(req, res) {
+         // Request includes the game ID, let's find it.
+         Game.findOne( { 'id': req.param('id') }).done(
+          function(err, game) {
+           if (err) {
+            return console.log(err)
+          }
 
-       	    	// Found a game, what are its instances??
-       	    	GameInstance.find({ gameId : game.id}).done(
-       	    		function(err, gameInstances) {
-       	    			if (err) {
-       	    				return console.log(err)
-       	    			}
+         	    	// Found a game, what are its instances??
+         	    	GameInstance.find({ gameId : game.id}).done(
+         	    		function(err, gameInstances) {
+         	    			if (err) {
+         	    				return console.log(err)
+         	    			}
 
-                  for (var i = 0; i < gameInstances.length; i++) {
-					var gameInstance = gameInstances[i];
-					
-					gameInstance.playerNames = [ ]
-                    
-					for (var j = 0; j < gameInstance.playerIds.length; j++) {
+                    for (var i = 0; i < gameInstances.length; i++) {
+                     var gameInstance = gameInstances[i];
+                     
+                     gameInstance.playerNames = [ ]
+                     
+                     for (var j = 0; j < gameInstance.playerIds.length; j++) {
                       User.findOne( { 'id' : gameInstance.playerIds[j] } ).done(
                         function(err, user) {
                           if (err) {
@@ -51,23 +51,23 @@ module.exports = {
 
                           gameInstance.playerNames.push(user.name)
                         }
-                      )
+                        )
                     }
                   }
 
                   return res.view( { 'game': game, 'gameInstances': gameInstances })
                 }
-              )
-            }
-       	)
-   },
+                )
+}
+)
+},
 
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to GameController)
-   */
-  _config: {}
+    /**
+     * Overrides for the settings in `config/controllers.js`
+     * (specific to GameController)
+     */
+     _config: {}
 
-  
-};
+     
+   };
